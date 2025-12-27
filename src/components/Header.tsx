bc95ad8a8932f6700d3c321f17aeae3039ea5c3a
eleonora berylo <eleonoraberylo@gmail.com>
2025-12-26 22:46:16 -0500

    Remove blog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,6 @@ export function Header() {
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Blog', href: '/blog' },
     { name: 'Contact', href: '#contact' },
   ]
 
